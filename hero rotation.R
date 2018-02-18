745f897b7203129fb124b4a3beba434b0da0e7d6
jsc034 <joshua.cho@clevelandsas.org>
2018-02-17 21:26:20 -0800

    initial commit

---
+++ hero rotation.R
@@ -0,0 +1,15 @@
+#rm(list=ls()) #clears variables
+#cat("\014") #clears console || ctrl+L also works 
+#getwd()
+#setwd()
+library(readr)
+
+file <- "~/GitHub/HeroRotation/HotsData - Rotation.csv"
+DFA <- read_csv(file)  
+  
+  
+  
+  
+  
+  
+  
